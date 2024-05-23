3f38d4ccdcb9afa0234dd5f74fb4d4ff0f47c040
mariabailen <mariajbailenjimenez@gmail.com>
2024-05-23 08:39:49 +0200

    Add bootstrap css to jpa

--- src/main/java/com/mariabailen/simfood/controller/MainController.java
+++ src/main/java/com/mariabailen/simfood/controller/MainController.java
@@ -1,12 +1,12 @@
 package com.mariabailen.simfood.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@RestController
+@Controller
 public class MainController {
     
     @Value("${spring.application.name}")
