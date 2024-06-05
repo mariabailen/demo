dbf43b239092befcd8f105030f3ea364f63bbd8e
mariabailen <mariajbailenjimenez@gmail.com>
2024-06-05 08:37:12 +0200

    change th:insert by th:replace

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,3 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
